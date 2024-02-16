c40f7d0db4cf3b9892e867966a61d1b8b5b70afc
nkosko21 <nickoy7@gmail.com>
2024-02-16 12:42:32 -0500

    grammar

--- src/pages/projects/PersonalWebsite.tsx
+++ src/pages/projects/PersonalWebsite.tsx
@@ -6,8 +6,8 @@ export default function PersonalFunction() {
     return (
         <p>
             I made this website using TypeScript React and Mantine. I used TypeScript to make the website more maintainable and to 
-            help me catch errors before they happen, React to make the website more dynamic and to help me manage the state of the website,
-            and Mantine to help me make the website more accessible and to help me make the website look better. It's being hosted using AWS Amplify.
+            help me catch errors before they happen, React to make the website more dynamic,
+            and Mantine to help me make the website more accessible and look better. It's being hosted using AWS Amplify and a Route 53 Hosted Zone.
         </p>
     );
 }
