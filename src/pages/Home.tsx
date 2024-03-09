9873195bb3a0149c4e41d28e63df5dcf9384df23
nkosko21 <nickoy7@gmail.com>
2024-03-09 17:07:37 -0500

    Proffessional life fix

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Affix, Tooltip, Transition } from "@mantine/core";
+import { Affix, HoverCard, Tooltip, Transition, Text } from "@mantine/core";
 import React, { useEffect, useState } from "react";
 import NavigationBar from "../components/NavigationBar";
 import nick_about from '../images/nick_about.jpg';
@@ -7,6 +7,7 @@ import '../index.css';
 import nick_showing from '../images/nick_showing.png';
 import nick_shaking_hands from '../images/nick_shaking_hand.jpg';
 import SLS_logo from '../images/SLS_logo.png';
+import varsity_tutors_logo from '../images/varsity_tutors_logo.png';
 import '@mantine/carousel/styles.css';
 import { Carousel } from "@mantine/carousel";
 import InTheClassroom from "./student life carousel/InTheClassroom";
@@ -14,6 +15,7 @@ import OutsideTheClassroom from "./student life carousel/OutsideTheClassroom";
 import InLondon from "./student life carousel/InLondon";
 import { useWindowScroll } from "@mantine/hooks";
 import { IconChevronDown } from "@tabler/icons-react";
+import ProfessionalExperience from "./ProfessionalExperience";
 
 function Home() {
     const [scroll, scrollTo] = useWindowScroll();
@@ -26,9 +28,9 @@ function Home() {
     useEffect(() => {
         const handleScroll = () => {
         const scrollPosition = window.scrollY;
-        if (scrollPosition < 500) {
+        if (scrollPosition < 300) {
             setscrollSection(0);
-        } else if (scrollPosition < 1200) {
+        } else if (scrollPosition < 1000) {
             setscrollSection(1);
         } else {
             setscrollSection(2);
@@ -86,64 +88,53 @@ function Home() {
         </Affix>
         
         <section className="about-me-section" style={backgroundStyle}>
-            <div className="about-me-content" style={{ maxWidth: 'none' }}>
-                <h2 id="professional-title">Professional Life</h2>
+            {/* <div className="about-me-content" style={{ maxWidth: 'none' }}> */}
+                <h2 id="professional-title" style={{ textAlign: "center" }}>Professional Life</h2>
                 <div className="row" style={{ justifyContent: 'center', flexWrap: 'wrap', maxWidth: window.innerWidth }}>
-                    <Transition transition="slide-right" mounted={scroll.y > 235}>
-                        {(transitionStyles) => (
-                            <div className="col professional-container" style={{... transitionStyles}}>
-                                <div className="col professional-image-text">
-                                    {window.innerWidth > 600 &&
-                                    <Tooltip label="Me helping a student with their assignment">
-                                        <img className="professional-images" src={nick_showing} alt="Showing Students Curriculum"/>
-                                    </Tooltip>}
-                                    <h3 className="professional-title" style={{ margin: 0, marginTop: 5 }}>IT Manager</h3>
-                                    <p>
-                                        From April to August of 2023 I Worked as an IT Manager at Digital Ready, a non-profit organization in Boston, MA with the mission to teach
-                                        underserved boston youth STEM Curriculum. In this role, I was responsible for teaching the students IT Curriculum, such as Python, Databasing, and Networks.
-                                    </p>
-                                </div>
-                            </div>
-                        )}
-                    </Transition>
-                    <Transition transition="slide-up" mounted={scroll.y > 235}>
-                        {(transitionStyles) => (
-                            <div className="col professional-container" style={{...transitionStyles}}>
-                                <div className="col professional-image-text">
-                                    {window.innerWidth > 600 &&
-                                    <img className="professional-images" src={SLS_logo} alt="Sitting with Students"/>}
-                                    <h3 className="professional-title" style={{ margin: 0, marginTop: 5 }}>Software Developer</h3>
-                                    <p>
-                                        From September to December of 2023, I worked as a Software Developer at SLS Consulting, LLC. where I designed, developed, tested, 
-                                        and maintained internal automation tools using TypeScript, HTML, and Python to streamline and enhance business processes. I deployed and 
-                                        monitored applications on cloud platforms, primarily AWS and Heroku, and integrated the React-Django stack to achieve seamless 
-                                        front-end and back-end web application functionality. 
-                                    </p>
-                                </div>
-                            </div>
-                        )}
-                    </Transition>
-                    <Transition transition="slide-left" mounted={scroll.y > 235}>
-                        {(transitionStyles) => (
-                            <div className="col professional-container" style={{...transitionStyles}}>
-                                <div className="col professional-image-text">
-                                    {window.innerWidth > 600 &&
-                                    <Tooltip label="Me pictured with the Governer of Rhode Island Dan McKee">
-                                        <img className="professional-images" src={nick_shaking_hands} alt="Shaking Hands with the Governer of Rhode Island"/>
-                                    </Tooltip>}
-                                    <h3 className="professional-title" style={{ margin: 0, marginTop: 5 }}>Lifeguard/Lifeguard Manager</h3>
-                                    <p>
-                                        During the summers from 2018-2021, I worked as a Lifeguard at Roger Wheeler State Beach in Narragansett, Rhode Island where 
-                                        I was responsible for the safety of the patrons at the beach, performing various minor first aids and ocean rescues. The following summer
-                                        I returned as a Lifeguard Manager whose responsibilities included managing the lifeguard staff, scheduling, training new lifeguards and performing 
-                                        life-threatening first-aids and rescues.
-                                    </p>
-                                </div>
-                            </div>
-                        )}
-                    </Transition>
+                    <ProfessionalExperience 
+                        title={"IT Manager"} 
+                        location={"Boston, MA"} 
+                        company={"Digital Ready"}
+                        timeframe={"April 2023 - August 2023"} 
+                        picture={nick_showing} 
+                        details={"From April to August of 2023 I Worked as an IT Manager at Digital Ready, a non-profit organization in Boston, MA with the mission to teach underserved boston youth STEM Curriculum. In this role, I was responsible for teaching the students IT Curriculum, such as Python, Databasing, and Networks."} 
+                        label={"Nicholas helping a student with their assignment"}                        
+                    />
+                    <ProfessionalExperience 
+                        title={"Software Developer"} 
+                        location={"Boston, MA"} 
+                        company={"SLS Consulting, LLC"}
+                        timeframe={"September 2023 - December 2023"} 
+                        picture={SLS_logo} 
+                        details={
+                            "From September to December of 2023, I worked as a Software Developer at SLS Consulting, LLC. where I designed, developed, tested," +
+                            "and maintained internal automation tools using TypeScript, HTML, and Python to streamline and enhance business processes. I deployed and " + 
+                            "monitored applications on cloud platforms, primarily AWS and Heroku, and integrated the React-Django stack to achieve seamless " + 
+                            "front-end and back-end web application functionality."}                     
+                    />
+                    <ProfessionalExperience 
+                        title={"Lifeguard/Lifeguard Manager"} 
+                        location={"Narragansett, RI"} 
+                        company={"RI Dept. of Environmental Management"}
+                        timeframe={"Summers 2018 - December 2021"} 
+                        picture={nick_shaking_hands} 
+                        details={
+                            "During the summers from 2018-2021, I worked as a Lifeguard at Roger Wheeler State Beach in Narragansett, Rhode Island where " +
+                            "I was responsible for the safety of the patrons at the beach, performing various minor first aids and ocean rescues. The following summer" +
+                            "I returned as a Lifeguard Manager whose responsibilities included managing the lifeguard staff, scheduling, training new lifeguards and performing " +
+                            "life-threatening first-aids and rescues."} 
+                        label={"Nicholas pictured with the Governer of Rhode Island Dan McKee"}                        
+                    />
+                    <ProfessionalExperience 
+                        title={"Computer Science Tutor"} 
+                        location={"Boston, MA (remote)"} 
+                        company={"Varsity Tutors"}
+                        timeframe={"February 2024 - present"} 
+                        picture={varsity_tutors_logo} 
+                        details={"Recently I've been working as a tutor for Varsity Tutors as a freelance Computer Science tutor teaching pupils ages 15-20 computer science concepts such as programming, databases, and web design."}                  
+                    />
                 </div>
-            </div>
+            {/* </div> */}
         </section>
 
         <section style={backgroundStyle}>
---
+++ src/pages/ProfessionalExperience.tsx
@@ -0,0 +1,49 @@
+import { Tooltip } from "@mantine/core";
+import varsity_tutors_logo from '../images/varsity_tutors_logo.png';
+import React, { useState } from "react";    
+import './Home.css';
+import '../index.css';
+
+export default function ProfessionalExperience(props: {
+    title: string, 
+    location: string, 
+    company: string,
+    timeframe: string, 
+    picture: any, 
+    details: string,
+    label?: string,
+}) {
+    const [opened, setOpened] = useState<Boolean>(false);
+
+
+    return(
+        <div className="col professional-container" onClick={() => setOpened(!opened)}>
+            {opened ? 
+                <div className="col professional-image-text">
+                    {window.innerWidth > 600 &&
+                        <div className="col" style={{alignItems: "flex-start"}}>
+                            {props.picture === varsity_tutors_logo ? 
+                            <img className="professional-images" style={{ objectFit: 'contain' }} src={props.picture} id="pic"/>
+                            :
+                            <img className="professional-images" src={props.picture} id="pic"/>}
+                            <p style={{ fontSize: 10, marginLeft: 10 }}>{props.label}</p>
+                        </div>
+                    }
+                    <h3 className="professional-title" style={{ margin: 0, marginTop: 5 }}>{props.title}</h3>
+                    <p>
+                        {props.details}
+                    </p>
+                </div>
+            :
+                <div className="col professional-image-text">
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', padding: 15}}>
+                    <h3 className="professional-title">{props.title}</h3>
+                    <p>{props.company}</p>
+                    <p>{props.location}</p>
+                    <p>{props.timeframe}</p>
+                    </div>
+                </div>
+            }
+        </div>
+    )
+}
