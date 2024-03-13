a9fa27b901a9112179579eb0554d9af2fe8001ef
nkosko21 <nickoy7@gmail.com>
2024-03-13 11:48:45 -0400

    Abstractions

---
+++ src/pages/CarouselData.tsx
@@ -0,0 +1,91 @@
+import { Image } from "@mantine/core";
+import northeastern_classroom from '../images/Northeastern_Classroom.jpg';
+import './studentLife.css';
+import './Home.css';
+import st_pauls from '../images/st_pauls.jpg';
+import nick_tackle from '../images/Nick_Tackle.jpg';
+
+const carouselData: any[] = [
+    {
+        body: (
+            <div className="col">
+                <h1 className="student-life-titles">Inside The Classroom</h1>
+                <p style={{textAlign: window.innerWidth < 600 ? 'center': 'start', fontSize: window.innerWidth < 600 ? '.9rem': '1.1rem'}}>
+                    As a third year Computer Science Student, I've taken a variety of different classes ranging from Computer Science to Design.
+                    I'm currently taking a Web Development class where I'm learning how to build a full stack web application using React, Node.js, and MongoDB.
+                    I'm also enrolled in a Programming Languages course, where I'm learning the building blocks of function oriented languages using DrRacket, 
+                    even building our own programming language using Scheme. My favorite however is Fundamentals of Software Engineering, a course 
+                    which covers the software development lifecycle and real world software development standards. 
+                </p>
+                <p style={{textAlign: window.innerWidth < 600 ? 'center': 'start', fontSize: window.innerWidth < 600 ? '.9rem': '1.1rem'}}>
+                    I've also taken classes on Algorithms, Object Oriented Design, Networks & Distrubuted Systems, Discrete Structures, and Philosophy in Film.
+                </p>
+            </div>
+        ),
+        image: (
+            <Image
+                src={northeastern_classroom}
+                style={{ height: 576, width: 432, margin: 10 }}
+                fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+                radius="md"
+            />
+        )
+    },
+    {
+        body: (
+            <div className='col'>
+                <h1 className="student-life-titles">In London</h1>
+                <p style={{textAlign: window.innerWidth < 600 ? 'center': 'start', fontSize: window.innerWidth < 600 ? '.9rem': '1.1rem'}}>
+                    In the Fall of my 2021 I spent a semester abroad in London, England with Northeastern's NUin program. 
+                    During my time there I took classes on London culture and the Fundamentals of Computer Science, using DrRacket to learn the basics
+                    of function oriented languages. I was also elected as a Student Coordinator on the Student government board 
+                    where I helped organize community events and various administrative tasks.
+                </p>
+                <p style={{textAlign: window.innerWidth < 600 ? 'center': 'start', fontSize: window.innerWidth < 600 ? '.9rem': '1.1rem'}}>
+                    I also took the opportunity to travel around Europe, visiting places such as Paris, Edinburgh, Madrid, Barcelona, Geneva, and Aberdeen, 
+                    documenting my travels on video.
+                </p>
+            </div>
+        ),
+        image: (
+            <Image
+                src={st_pauls}
+                alt="A photo I took of St.Paul's Cathedral"
+                style={{ height: 576, width: 432, margin: 10 }}
+                fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+                radius="md"
+            />
+        )
+    }, 
+    {
+        body: (
+            <div className='col'>
+                <h1 className='student-life-titles'>Outside The Classroom</h1>
+                <p style={{textAlign: window.innerWidth < 600 ? 'center': 'start', fontSize: window.innerWidth < 600 ? '.9rem': '1.1rem'}}>
+                    I do a variety of different clubs and personal projects. I was on the Northeastern Club swim team for 3 years,
+                    coming .01 seconds away from reaching the National Championships in the 100m freestyle before suffering an injury, requiring surgery and postponing 
+                    any swimming for the foreseeable future. 
+                </p>
+                <p style={{textAlign: window.innerWidth < 600 ? 'center': 'start', fontSize: window.innerWidth < 600 ? '.9rem': '1.1rem'}}>
+                    I also was a founder of 'Rare Fish Enthusiasts', a team where we participate in various sports activities and are 
+                    currently competing in Northeastern Intramural Soccer. I run our {<a href='https://www.instagram.com/rarefishenthusiasts/' target="_blank">Social Media</a>} where I post 
+                    various content I make from silly highlight videos to graphics. I also have a {<a href='https://www.instagram.com/7nkfilms/' target="_blank">Personal Instagram</a>} where
+                    I post my personal videos and photography.
+                </p>
+                <p style={{textAlign: window.innerWidth < 600 ? 'center': 'start', fontSize: window.innerWidth < 600 ? '.9rem': '1.1rem'}}>
+                    Finally, I volunteer on weekends as an Assistant Youth Soccer Coach at VOLO Sports, helping underserved children ages 5-10 learn the fundamentals of Soccer
+                    and teamwork.
+                </p>
+            </div>
+        ),
+        image: (
+            <Image
+                src={nick_tackle}
+                style={{ height: 576, width: 432, margin: 10 }}
+                fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+                radius="md"
+            />
+        )
+    }
+]
+export default carouselData
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,13 +9,11 @@ import nick_shaking_hands from '../images/nick_shaking_hand.jpg';
 import SLS_logo from '../images/SLS_logo.png';
 import varsity_tutors_logo from '../images/varsity_tutors_logo.png';
 import '@mantine/carousel/styles.css';
-import { Carousel } from "@mantine/carousel";
-import InTheClassroom from "./student life carousel/InTheClassroom";
-import OutsideTheClassroom from "./student life carousel/OutsideTheClassroom";
-import InLondon from "./student life carousel/InLondon";
+import { Carousel, CarouselSlide } from "@mantine/carousel";
 import { useWindowScroll } from "@mantine/hooks";
 import { IconChevronDown } from "@tabler/icons-react";
 import ProfessionalExperience from "./ProfessionalExperience";
+import carouselData from "./CarouselData";
 
 function Home() {
     const [scroll, scrollTo] = useWindowScroll();
@@ -24,6 +22,7 @@ function Home() {
     const professionalTitle = document.getElementById("professional-title");
     const studentLife = document.getElementById("student-life-title");
     const [isVisible, setIsVisible] = useState(false);
+    console.log(carouselData)
 
     useEffect(() => {
         const handleScroll = () => {
@@ -88,53 +87,51 @@ function Home() {
         </Affix>
         
         <section className="about-me-section" style={backgroundStyle}>
-            {/* <div className="about-me-content" style={{ maxWidth: 'none' }}> */}
-                <h2 id="professional-title" style={{ textAlign: "center" }}>Professional Experience</h2>
-                <div className="row" style={{ justifyContent: 'center', flexWrap: 'wrap', maxWidth: window.innerWidth }}>
-                    <ProfessionalExperience 
-                        title={"IT Manager"} 
-                        location={"Boston, MA"} 
-                        company={"Digital Ready"}
-                        timeframe={"April 2023 - August 2023"} 
-                        picture={nick_showing} 
-                        details={"From April to August of 2023 I Worked as an IT Manager at Digital Ready, a non-profit organization in Boston, MA with the mission to teach underserved boston youth STEM Curriculum. In this role, I was responsible for teaching the students IT Curriculum, such as Python, Databasing, and Networks."} 
-                        label={"Nicholas helping a student with their assignment"}                        
-                    />
-                    <ProfessionalExperience 
-                        title={"Software Developer"} 
-                        location={"Boston, MA"} 
-                        company={"SLS Consulting, LLC"}
-                        timeframe={"September 2023 - December 2023"} 
-                        picture={SLS_logo} 
-                        details={
-                            "From September to December of 2023, I worked as a Software Developer at SLS Consulting, LLC. where I designed, developed, tested," +
-                            "and maintained internal automation tools using TypeScript, HTML, and Python to streamline and enhance business processes. I deployed and " + 
-                            "monitored applications on cloud platforms, primarily AWS and Heroku, and integrated the React-Django stack to achieve seamless " + 
-                            "front-end and back-end web application functionality."}                     
-                    />
-                    <ProfessionalExperience 
-                        title={"Lifeguard/Lifeguard Manager"} 
-                        location={"Narragansett, RI"} 
-                        company={"RI Dept. of Environmental Management"}
-                        timeframe={"Summers 2018 - December 2021"} 
-                        picture={nick_shaking_hands} 
-                        details={
-                            "During the summers from 2018-2021, I worked as a Lifeguard at Roger Wheeler State Beach in Narragansett, Rhode Island where " +
-                            "I was responsible for the safety of the patrons at the beach, performing various minor first aids and ocean rescues. The following summer" +
-                            "I returned as a Lifeguard Manager whose responsibilities included managing the lifeguard staff, scheduling, training new lifeguards and performing " +
-                            "life-threatening first-aids and rescues."} 
-                        label={"Nicholas pictured with the Governer of Rhode Island Dan McKee"}                        
-                    />
-                    <ProfessionalExperience 
-                        title={"Computer Science Tutor"} 
-                        location={"Boston, MA (remote)"} 
-                        company={"Varsity Tutors"}
-                        timeframe={"February 2024 - present"} 
-                        picture={varsity_tutors_logo} 
-                        details={"Recently I've been working as a tutor for Varsity Tutors as a freelance Computer Science tutor teaching pupils ages 15-20 computer science concepts such as programming, databases, and web design."}                  
-                    />
-                </div>
-            {/* </div> */}
+            <h2 id="professional-title" style={{ textAlign: "center" }}>Professional Experience</h2>
+            <div className="row" style={{ justifyContent: 'center', flexWrap: 'wrap', maxWidth: window.innerWidth }}>
+                <ProfessionalExperience 
+                    title={"IT Manager"} 
+                    location={"Boston, MA"} 
+                    company={"Digital Ready"}
+                    timeframe={"April 2023 - August 2023"} 
+                    picture={nick_showing} 
+                    details={"From April to August of 2023 I Worked as an IT Manager at Digital Ready, a non-profit organization in Boston, MA with the mission to teach underserved boston youth STEM Curriculum. In this role, I was responsible for teaching the students IT Curriculum, such as Python, Databasing, and Networks."} 
+                    label={"Nicholas helping a student with their assignment"}                        
+                />
+                <ProfessionalExperience 
+                    title={"Software Developer"} 
+                    location={"Boston, MA"} 
+                    company={"SLS Consulting, LLC"}
+                    timeframe={"September 2023 - December 2023"} 
+                    picture={SLS_logo} 
+                    details={
+                        "From September to December of 2023, I worked as a Software Developer at SLS Consulting, LLC. where I designed, developed, tested," +
+                        "and maintained internal automation tools using TypeScript, HTML, and Python to streamline and enhance business processes. I deployed and " + 
+                        "monitored applications on cloud platforms, primarily AWS and Heroku, and integrated the React-Django stack to achieve seamless " + 
+                        "front-end and back-end web application functionality."}                     
+                />
+                <ProfessionalExperience 
+                    title={"Lifeguard/Lifeguard Manager"} 
+                    location={"Narragansett, RI"} 
+                    company={"RI Dept. of Environmental Management"}
+                    timeframe={"Summers 2018 - December 2021"} 
+                    picture={nick_shaking_hands} 
+                    details={
+                        "During the summers from 2018-2021, I worked as a Lifeguard at Roger Wheeler State Beach in Narragansett, Rhode Island where " +
+                        "I was responsible for the safety of the patrons at the beach, performing various minor first aids and ocean rescues. The following summer" +
+                        "I returned as a Lifeguard Manager whose responsibilities included managing the lifeguard staff, scheduling, training new lifeguards and performing " +
+                        "life-threatening first-aids and rescues."} 
+                    label={"Nicholas pictured with the Governer of Rhode Island Dan McKee"}                        
+                />
+                <ProfessionalExperience 
+                    title={"Computer Science Tutor"} 
+                    location={"Boston, MA (remote)"} 
+                    company={"Varsity Tutors"}
+                    timeframe={"February 2024 - present"} 
+                    picture={varsity_tutors_logo} 
+                    details={"Recently I've been working as a tutor for Varsity Tutors as a freelance Computer Science tutor teaching pupils ages 15-20 computer science concepts such as programming, databases, and web design."}                  
+                />
+            </div>
         </section>
 
         <section style={backgroundStyle}>
@@ -144,15 +141,16 @@ function Home() {
                 <h2 id="student-life-title">Student Life</h2>
             </div>  
             <Carousel slideSize="70%" align={window.innerWidth < 600 ? "center": "center"} loop slideGap="xl">
-                <Carousel.Slide>
-                    <InTheClassroom/>
-                </Carousel.Slide>
-                <Carousel.Slide>
-                    <OutsideTheClassroom/>
-                </Carousel.Slide>
-                <Carousel.Slide>
-                    <InLondon/>
-                </Carousel.Slide>
+                {carouselData.map((slide) => 
+                    <Carousel.Slide>
+                        <div className="student-life-container">
+                            <div className='row'>
+                                {slide.body}
+                                {window.innerWidth > 600 && slide.image}
+                            </div>
+                        </div>
+                    </Carousel.Slide>
+                )}
             </Carousel>
         </section>
     </div>
